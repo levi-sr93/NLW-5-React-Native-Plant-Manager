cc5dca13024f7a61e07d89a649a3bae4266ef370
levi-sr93 <levi-sr@outlook.com>
2021-04-24 22:25:27 -0300

    Ouvindo todas as notificaçoes agendadas

--- App.tsx
+++ App.tsx
@@ -21,14 +21,20 @@ export default function App() {
   });
 
   useEffect(() => {
-    const subscription = Notifications.addNotificationReceivedListener(
-      async (notification) => {
-        const data = notification.request.content.data.plant as PlantProps;
-        console.log(data);
-      }
-    );
+    // const subscription = Notifications.addNotificationReceivedListener(
+    //   async (notification) => {
+    //     const data = notification.request.content.data.plant as PlantProps;
+    //     console.log(data);
+    //   }
+    // );
 
-    return () => subscription.remove();
+    // return () => subscription.remove();
+
+    async function notifications() {
+      const data = await Notifications.getAllScheduledNotificationsAsync();
+      console.log("Notificações agendadas", data);
+    }
+    notifications();
   }, []);
 
   if (!fontsLoaded) {
