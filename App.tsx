051c4da52997cd375756ea5d1703c3a71b65831d
levi-sr93 <levi-sr@outlook.com>
2021-04-24 22:21:47 -0300

    Funçao que fica escultando o evento de notificaçao e traz o payload

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
 import "react-native-gesture-handler";
-import AppLoading from "expo-app-loading";
-import React from "react";
+import React, { useEffect } from "react";
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold,
 } from "@expo-google-fonts/jost";
+import AppLoading from "expo-app-loading";
+
+import * as Notifications from "expo-notifications";
 
 import { StyleSheet } from "react-native";
 
 import Routes from "./src/routes";
+import { PlantProps } from "./src/libs/storage";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold,
   });
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async (notification) => {
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data);
+      }
+    );
+
+    return () => subscription.remove();
+  }, []);
 
   if (!fontsLoaded) {
     return <AppLoading />;
